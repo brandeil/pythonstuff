44848c9939b309481d4df1aad127ec1185b15ddd
brandeil <me@gmail.com>
2018-06-10 06:02:33 -0400

    more

--- filestuff.py
+++ filestuff.py
@@ -1,6 +1,25 @@
 filename="c:\\users\\len\\documents\\aws_adminwebconsole.txt"
 
+#read entire file at once
 with open(filename) as f_obj:
     contents=f_obj.read()
 
 print(contents)
+
+#read file line by line
+with open(filename) as f_obj:
+    for line in f_obj:
+        if line != "\n":
+            print(line.rstrip())
+
+#storing the lines in a list
+myfile=[]
+with open(filename) as f_obj:
+    myfile=f_obj.readlines()
+
+print(len(myfile))
+print(myfile)        
+
+#writing to a file
+with open('somefile','w') as f:
+   f.write("mystuff to write")
---
+++ tryexcept.py
@@ -0,0 +1,4 @@
+try:
+         print(5/0)
+except ZeroDivisionError:  #<<<the tracebook tells you what kind of exception to check for
+     print("cannot divide by zero")    
